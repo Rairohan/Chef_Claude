7378552259110eeb178fc8f4cce2a8209744759b
rohan <rairohan14303@gmail.com>
2025-12-16 21:34:10 +0545

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import '/src/index.css'
+export default function Header(){
+    return(
+        <header>
+            <img src="\public\chef-claude-icon.png" alt="Chef Claude logo" className='logo'/>
+            <h1>Chef Claude</h1>
+        </header>  
+    )
+}
---
+++ components/Main.jsx
@@ -0,0 +1,15 @@
+import '../src/index.css'
+export default function Main(){
+    return(
+    <main>
+        <form>
+            <input
+            type='text'
+            placeholder=' e.g oregano'
+            aria-label='Add ingredient'
+            /> 
+            <button>+ Add ingredients</button>
+        </form>
+    </main>
+    )
+} 
