5c8d8fd1b0358544dcf7cb12fe9992f7599c92cc
rohan <rairohan14303@gmail.com>
2025-12-17 21:51:22 +0545

    changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,17 @@
 import '../src/index.css'
 export default function Main(){
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("submitted")
+    }
     return(
     <main>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
             type='text'
             placeholder=' e.g oregano'
             aria-label='Add ingredient'
+            name='ingredient'
             /> 
             <button>+ Add ingredients</button>
         </form>
